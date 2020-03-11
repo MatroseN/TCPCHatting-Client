2a1dd5d27629e0a8a40421754073fe15cd0d528b
felix <felix.lundh@gmail.com>
2020-03-11 21:35:04 +0100

    fix

--- src/ClientDriver.java
+++ src/ClientDriver.java
@@ -7,13 +7,18 @@ public static void main(String[] args){
 
     Client client;
     Client client2;
+    Client client3;
 
-     client = new Client(serverString, serverPort, "Hello from client " + name, name);
+    client = new Client(serverString, serverPort, "Hello from client " + name, name);
 
     client.start();
 
     client2 = new Client(serverString, serverPort, "Hello from client " + "test", "test");
 
     client2.start();
+
+    client3 = new Client(serverString, serverPort, "Hello from client " + "Felix", "Felix");
+
+    client3.start();
     }
 }
